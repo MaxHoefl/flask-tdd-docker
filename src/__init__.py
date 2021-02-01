953de4e2f59786bc825a2de8872b93d794666862
MaxHoefl <ucabhoe@ucl.ac.uk>
2021-02-01 20:50:35 +0100

    adding blueprint

--- src/__init__.py
+++ src/__init__.py
@@ -3,31 +3,32 @@ from flask import Flask, jsonify
 from flask_restx import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-api = Api(app)
-app_settings = os.getenv('APP_SETTINGS')
-app.config.from_object(app_settings)
-db = SQLAlchemy(app)
+
+db = SQLAlchemy()
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.String(128), nullable=False)
-    email = db.Column(db.String(128), nullable=False)
-    active = db.Column(db.Boolean, default=True, nullable=False)
+def create_app(script_info=None):
+    app = Flask(__name__)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
 
+    # integrate db to our app
+    db.init_app(app)
 
-class Ping(Resource):
-    def get(self):
+    # register blueprints
+    from src.api.ping import ping_blueprint
+    app.register_blueprint(ping_blueprint)
+
+    @app.shell_context_processor
+    def ctx():
         return {
-            'status': 'success',
-            'message': 'pong!'
+            'app': app,
+            'db': db
         }
 
+    return app
 
-api.add_resource(Ping, '/ping')
+
+
